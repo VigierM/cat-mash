1fc9be70c8150fbaaa2a0cd051e0b6cb925520c7
vigierM <vigier_b@etna-alternance.net>
2023-08-07 17:10:24 +0200

    Initialisation de l'application

---
+++ cat-mash-app/layouts/default.vue
@@ -0,0 +1,48 @@
+<template>
+ <v-app>
+    <v-app-bar
+      :clipped-left="clipped"
+      fixed
+      app
+    >
+      <v-container fill-height>
+        <v-row justify="center" align="center">
+          <v-col>
+            <h2 class="text-center">
+              Cat Mash
+            </h2>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-app-bar>
+    <v-main>
+      <div>
+        <Nuxt />
+      </div>
+    </v-main>
+    <v-footer
+      :absolute="!fixed"
+      app
+    >
+      <span>&copy; Sweetie Inc Cat Mash {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+  data () {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Cat Mash'
+    }
+  }
+}
+</script>
